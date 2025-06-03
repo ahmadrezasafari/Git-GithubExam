39a1a5a62325f6bbc128935f984278866ff07243
Ahmadreza <amirsafari1516@gmail.com>
2025-06-03 11:33:51 +0330

    changing the file

--- DisplaySimpleFigureWithJava.java
+++ DisplaySimpleFigureWithJava.java
@@ -9,22 +9,17 @@ public class DisplaySimpleFigureWithJava {
     public static void ninety() {
         System.out.println("* | | | | | *");
     }
-<<<<<<< HEAD:DisplaySimpleFigureWithJava-InitialVersion.java
     
     public static void fourStar() {
         System.out.println("    *****    ");
     }
-}
-=======
 
     public static void stars() {
         System.out.println("    *****    ");
         System.out.println("  *********  ");
         System.out.println("*************");
     }
-  
-    }
->>>>>>> stars-branch:DisplaySimpleFigureWithJava.java
+   }
 
 
 
